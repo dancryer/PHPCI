f4520751aa205f6015008418e1f04e2ab61f583f
Kris Wallsmith <kris.wallsmith@gmail.com>
2011-03-11 07:40:19 -0800

    removed unused vendors

--- app/autoload.php
+++ app/autoload.php
@@ -4,18 +4,14 @@ use Symfony\Component\ClassLoader\UniversalClassLoader;
 
 $loader = new UniversalClassLoader();
 $loader->registerNamespaces(array(
-    'Symfony'                        => array(__DIR__.'/../vendor/symfony/src', __DIR__.'/../vendor/bundles'),
-    'Sensio'                         => __DIR__.'/../vendor/bundles',
-    'Doctrine\\Common\\DataFixtures' => __DIR__.'/../vendor/doctrine-data-fixtures/lib',
-    'Doctrine\\Common'               => __DIR__.'/../vendor/doctrine-common/lib',
-    'Doctrine\\DBAL\\Migrations'     => __DIR__.'/../vendor/doctrine-migrations/lib',
-    'Doctrine\\MongoDB'              => __DIR__.'/../vendor/doctrine-mongodb/lib',
-    'Doctrine\\ODM\\MongoDB'         => __DIR__.'/../vendor/doctrine-mongodb-odm/lib',
-    'Doctrine\\DBAL'                 => __DIR__.'/../vendor/doctrine-dbal/lib',
-    'Doctrine'                       => __DIR__.'/../vendor/doctrine/lib',
-    'Zend\\Log'                      => __DIR__.'/../vendor/zend-log',
-    'Assetic'                        => __DIR__.'/../vendor/assetic/src',
-    'Acme'                           => __DIR__.'/../src',
+    'Symfony'          => array(__DIR__.'/../vendor/symfony/src', __DIR__.'/../vendor/bundles'),
+    'Sensio'           => __DIR__.'/../vendor/bundles',
+    'Doctrine\\Common' => __DIR__.'/../vendor/doctrine-common/lib',
+    'Doctrine\\DBAL'   => __DIR__.'/../vendor/doctrine-dbal/lib',
+    'Doctrine'         => __DIR__.'/../vendor/doctrine/lib',
+    'Zend\\Log'        => __DIR__.'/../vendor/zend-log',
+    'Assetic'          => __DIR__.'/../vendor/assetic/src',
+    'Acme'             => __DIR__.'/../src',
 ));
 $loader->registerPrefixes(array(
     'Twig_Extensions_' => __DIR__.'/../vendor/twig-extensions/lib',

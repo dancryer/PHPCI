b4094ed631dea84c751cead2503ae6e7ec03de62
Fabien Potencier <fabien.potencier@gmail.com>
2011-04-28 14:49:50 +0200

    tweaked front controllers to enable PHP class caches

--- web/app.php
+++ web/app.php
@@ -9,4 +9,5 @@ use Symfony\Component\HttpFoundation\Request;
 
 //$kernel = new AppCache(new AppKernel('prod', false));
 $kernel = new AppKernel('prod', false);
+$kernel->loadClassCache();
 $kernel->handle(Request::createFromGlobals())->send();
--- web/app_dev.php
+++ web/app_dev.php
@@ -16,4 +16,5 @@ require_once __DIR__.'/../app/AppKernel.php';
 use Symfony\Component\HttpFoundation\Request;
 
 $kernel = new AppKernel('dev', true);
+$kernel->loadClassCache();
 $kernel->handle(Request::createFromGlobals())->send();

885639aae29ccc3fe79d8e529257dbff2ccdd3a0
Fabien Potencier <fabien.potencier@gmail.com>
2011-07-21 22:10:44 +0200

    tweaked app.php when using the built-in reverse proxy

--- web/app.php
+++ web/app.php
@@ -6,7 +6,7 @@ require_once __DIR__.'/../app/AppKernel.php';
 
 use Symfony\Component\HttpFoundation\Request;
 
-//$kernel = new AppCache(new AppKernel('prod', false));
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
+//$kernel = new AppCache($kernel);
 $kernel->handle(Request::createFromGlobals())->send();

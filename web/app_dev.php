5cc38327684de286c581993fa1eef95c3449e888
stealth35 <jinmoku@hotmail.com>
2011-07-25 18:02:55 +0200

    removed alias (die to exit)

--- web/app_dev.php
+++ web/app_dev.php
@@ -7,7 +7,7 @@ if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
     '::1',
 ))) {
     header('HTTP/1.0 403 Forbidden');
-    die('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
 require_once __DIR__.'/../app/bootstrap.php.cache';
--- web/config.php
+++ web/config.php
@@ -1,7 +1,7 @@
 <?php
 
 if (!isset($_SERVER['HTTP_HOST'])) {
-    die('This script cannot be run from the CLI. Run it from a browser.');
+    exit('This script cannot be run from the CLI. Run it from a browser.');
 }
 
 if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
@@ -9,7 +9,7 @@ if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
     '::1',
 ))) {
     header('HTTP/1.0 403 Forbidden');
-    die('This script is only accessible from localhost.');
+    exit('This script is only accessible from localhost.');
 }
 
 $majorProblems = array();

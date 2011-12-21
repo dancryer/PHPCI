31031aea502b43257c11779d65a9c5988a99e14f
Jordi Boggiano <j.boggiano@seld.be>
2011-12-21 17:04:07 +0100

    Fix feedback

--- app/autoload.php
+++ app/autoload.php
@@ -2,9 +2,18 @@
 
 if (!$loader = include __DIR__.'/../vendor/.composer/autoload.php') {
     $nl = PHP_SAPI === 'cli' ? PHP_EOL : '<br />';
-    die('You must set up the project dependencies, run the following commands:'.PHP_EOL.
-        'curl -s http://getcomposer.org/installer | php'.PHP_EOL.
-        'php composer.phar install'.PHP_EOL);
+    echo "$nl$nl";
+    if (is_writable(dirname(__DIR__))) {
+        file_put_contents(dirname(__DIR__).'/composer.phar', file_get_contents('http://getcomposer.org/composer.phar'));
+        die("You must set up the project dependencies.$nl".
+            "Composer has been downloaded.$nl".
+            "Run the following command in ".dirname(__DIR__).":$nl$nl".
+            "php composer.phar install$nl");
+    }
+    die("You must set up the project dependencies.$nl".
+        "Run the following commands in ".dirname(__DIR__).":$nl$nl".
+        "wget http://getcomposer.org/composer.phar$nl".
+        "php composer.phar install$nl");
 }
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
@@ -32,4 +41,4 @@ AnnotationRegistry::registerFile(__DIR__.'/../vendor/doctrine/orm/lib/Doctrine/O
 // Swiftmailer needs a special autoloader to allow
 // the lazy loading of the init file (which is expensive)
 require_once __DIR__.'/../vendor/swiftmailer/swiftmailer/lib/classes/Swift.php';
-Swift::registerAutoload(__DIR__.'/../vendor/swiftmailer/lib/swift_init.php');
+Swift::registerAutoload(__DIR__.'/../vendor/swiftmailer/swiftmailer/lib/swift_init.php');

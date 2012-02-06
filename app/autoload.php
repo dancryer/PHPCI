81c96cd75e50cbc9a29af38b6a6085a343095c34
Jordi Boggiano <j.boggiano@seld.be>
2012-02-06 18:58:08 +0100

    Update install-helper code

--- app/autoload.php
+++ app/autoload.php
@@ -3,17 +3,19 @@
 if (!$loader = include __DIR__.'/../vendor/.composer/autoload.php') {
     $nl = PHP_SAPI === 'cli' ? PHP_EOL : '<br />';
     echo "$nl$nl";
-    if (is_writable(dirname(__DIR__))) {
-        file_put_contents(dirname(__DIR__).'/composer.phar', file_get_contents('http://getcomposer.org/composer.phar'));
-        die("You must set up the project dependencies.$nl".
-            "Composer has been downloaded.$nl".
-            "Run the following command in ".dirname(__DIR__).":$nl$nl".
-            "php composer.phar install$nl");
+    if (is_writable(dirname(__DIR__)) && $installer = @file_get_contents('http://getcomposer.org/installer')) {
+        echo 'You must set up the project dependencies.'.$nl;
+        $installerPath = dirname(__DIR__).'/install-composer.php';
+        file_put_contents($installerPath, $installer);
+        echo 'The composer installer has been downloaded in '.$installerPath.$nl;
+        die('Run the following commands in '.dirname(__DIR__).':'.$nl.$nl.
+            'php install-composer.php'.$nl.
+            'php composer.phar install'.$nl);
     }
-    die("You must set up the project dependencies.$nl".
-        "Run the following commands in ".dirname(__DIR__).":$nl$nl".
-        "wget http://getcomposer.org/composer.phar$nl".
-        "php composer.phar install$nl");
+    die('You must set up the project dependencies.'.$nl.
+        'Run the following commands in '.dirname(__DIR__).':'.$nl.$nl.
+        'curl -s http://getcomposer.org/installer | php'.$nl.
+        'php composer.phar install'.$nl);
 }
 
 use Doctrine\Common\Annotations\AnnotationRegistry;

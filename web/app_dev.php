edd3e201371583b3e20110fec9b4fb1a765bf170
Jordi Boggiano <j.boggiano@seld.be>
2012-02-07 14:40:04 +0100

    Restore use of the bootstrap file

--- web/app_dev.php
+++ web/app_dev.php
@@ -14,7 +14,7 @@ if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
-require_once __DIR__.'/../app/autoload.php';
+require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
 
 use Symfony\Component\HttpFoundation\Request;

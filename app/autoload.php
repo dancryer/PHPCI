c80b7a3d928aab83d4331d661a45769ee42746ab
Jordi Boggiano <j.boggiano@seld.be>
2012-02-09 18:29:35 +0100

    Cleanups

--- app/autoload.php
+++ app/autoload.php
@@ -24,10 +24,7 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 if (!function_exists('intl_get_error_code')) {
     require_once __DIR__.'/../vendor/symfony/src/Symfony/Component/Locale/Resources/stubs/functions.php';
 
-    $loader->add('IntlDateFormatter', __DIR__.'/../vendor/symfony/src/Symfony/Component/Locale/Resources/stubs');
-    $loader->add('Collator', __DIR__.'/../vendor/symfony/src/Symfony/Component/Locale/Resources/stubs');
-    $loader->add('Locale', __DIR__.'/../vendor/symfony/src/Symfony/Component/Locale/Resources/stubs');
-    $loader->add('NumberFormatter', __DIR__.'/../vendor/symfony/src/Symfony/Component/Locale/Resources/stubs');
+    $loader->add('', __DIR__.'/../vendor/symfony/src/Symfony/Component/Locale/Resources/stubs');
 }
 
 if (!interface_exists('SessionHandlerInterface', false)) {
@@ -38,7 +35,6 @@ AnnotationRegistry::registerLoader(function($class) use ($loader) {
     $loader->loadClass($class);
     return class_exists($class, false);
 });
-AnnotationRegistry::registerFile(__DIR__.'/../vendor/doctrine/orm/lib/Doctrine/ORM/Mapping/Driver/DoctrineAnnotations.php');
 
 // Swiftmailer needs a special autoloader to allow
 // the lazy loading of the init file (which is expensive)

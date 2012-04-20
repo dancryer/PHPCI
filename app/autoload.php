b076ed43d756bd1a9faafeeec124c9feac2f6c1f
Fabien Potencier <fabien.potencier@gmail.com>
2012-04-20 06:47:40 +0200

    removed the automatic downloading of composer in autoload.php

--- app/autoload.php
+++ app/autoload.php
@@ -3,15 +3,6 @@
 if (!$loader = include __DIR__.'/../vendor/autoload.php') {
     $nl = PHP_SAPI === 'cli' ? PHP_EOL : '<br />';
     echo "$nl$nl";
-    if (is_writable(dirname(__DIR__)) && $installer = @file_get_contents('http://getcomposer.org/installer')) {
-        echo 'You must set up the project dependencies.'.$nl;
-        $installerPath = dirname(__DIR__).'/install-composer.php';
-        file_put_contents($installerPath, $installer);
-        echo 'The composer installer has been downloaded in '.$installerPath.$nl;
-        die('Run the following commands in '.dirname(__DIR__).':'.$nl.$nl.
-            'php install-composer.php'.$nl.
-            'php composer.phar install'.$nl);
-    }
     die('You must set up the project dependencies.'.$nl.
         'Run the following commands in '.dirname(__DIR__).':'.$nl.$nl.
         'curl -s http://getcomposer.org/installer | php'.$nl.

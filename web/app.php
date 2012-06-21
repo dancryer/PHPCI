c3f2698e6d1188bb7b2c838a7f1e6b78b419d3a3
Fabien Potencier <fabien.potencier@gmail.com>
2012-06-21 14:59:46 +0200

    fixed typo

--- web/app.php
+++ web/app.php
@@ -10,7 +10,6 @@ $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 /*
 $loader = new ApcClassLoader('sf2', $loader);
 $loader->register(true);
-}
 */
 
 require_once __DIR__.'/../app/AppKernel.php';

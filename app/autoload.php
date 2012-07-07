94dccf163c480016038f46070dd70887b3e29abc
Fabien Potencier <fabien.potencier@gmail.com>
2012-07-07 16:53:59 +0200

    removed error message in autoload.php as the check is already be done in check.php and config.php

--- app/autoload.php
+++ app/autoload.php
@@ -2,23 +2,7 @@
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
 
-if (!$loader = @include __DIR__.'/../vendor/autoload.php') {
-
-    $message = <<< EOF
-<p>You must set up the project dependencies by running the following commands:</p>
-<pre>
-    curl -s http://getcomposer.org/installer | php
-    php composer.phar install
-</pre>
-
-EOF;
-
-    if (PHP_SAPI === 'cli') {
-        $message = strip_tags($message);
-    }
-
-    die($message);
-}
+$loader = include __DIR__.'/../vendor/autoload.php';
 
 // intl
 if (!function_exists('intl_get_error_code')) {

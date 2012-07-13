5bd76ae569fd07eab33a4aec221b0a5e81a6b8d6
Fabien Potencier <fabien.potencier@gmail.com>
2012-07-13 21:23:29 +0200

    raised the minimum version of PHP

--- app/SymfonyRequirements.php
+++ app/SymfonyRequirements.php
@@ -376,7 +376,7 @@ class RequirementCollection implements IteratorAggregate
  */
 class SymfonyRequirements extends RequirementCollection
 {
-    const REQUIRED_PHP_VERSION = '5.3.3';
+    const REQUIRED_PHP_VERSION = '5.3.4';
 
     /**
      * Constructor that initializes the requirements.

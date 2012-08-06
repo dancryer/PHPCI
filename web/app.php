e3e652a27e127a7ffb764294f4cf2d6e2f1a1578
Andreas Fischer <bantu@phpbb.com>
2012-08-06 16:33:51 +0200

    Improve wording of the comment about ApcClassLoader in app.php

--- web/app.php
+++ web/app.php
@@ -5,8 +5,9 @@ use Symfony\Component\HttpFoundation\Request;
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
-// Use APC for autoloading to improve performance
-// Change 'sf2' by the prefix you want in order to prevent key conflict with another application
+// Use APC for autoloading to improve performance.
+// Change 'sf2' to a unique prefix in order to prevent cache key conflicts
+// with other applications also using APC.
 /*
 $loader = new ApcClassLoader('sf2', $loader);
 $loader->register(true);

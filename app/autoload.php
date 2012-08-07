cc5c5befde0ffa7bb0943a5f64d06b832d25f112
Jordan Alliot <jordan.alliot@gmail.com>
2012-08-08 00:24:58 +0200

    Remove specific autoloading part to reflect latest Symfony master

--- app/autoload.php
+++ app/autoload.php
@@ -7,8 +7,6 @@ $loader = require __DIR__.'/../vendor/autoload.php';
 // intl
 if (!function_exists('intl_get_error_code')) {
     require_once __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs/functions.php';
-
-    $loader->add('', __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs');
 }
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));

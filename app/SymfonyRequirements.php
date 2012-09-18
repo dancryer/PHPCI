dff325057723643842cd25c2c4abb44885ce2412
Fabien Potencier <fabien.potencier@gmail.com>
2012-09-18 13:29:44 +0200

    finished reverting previous changes

--- app/SymfonyRequirements.php
+++ app/SymfonyRequirements.php
@@ -477,13 +477,7 @@ class SymfonyRequirements extends RequirementCollection
 
         $this->addPhpIniRequirement('detect_unicode', false);
 
-        ob_start();
-        phpinfo();
-        $phpinfo = ob_get_contents();
-        ob_end_clean();
-
-        // the phpinfo check is necessary when Suhosin is compiled into PHP
-        if (extension_loaded('suhosin') || false !== strpos($phpinfo, 'Suhosin')) {
+        if (extension_loaded('suhosin')) {
             $this->addPhpIniRequirement(
                 'suhosin.executor.include.whitelist',
                 create_function('$cfgValue', 'return false !== stripos($cfgValue, "phar");'),

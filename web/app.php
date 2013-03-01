1970b831f8843a5cf551e9d88404cb62f21b90f9
Fabien Potencier <fabien.potencier@gmail.com>
2013-03-01 07:38:17 +0100

    enabled the HTTP method override by default to be more BC (closes sensio/SensioGeneratorBundle#165)

--- web/app.php
+++ web/app.php
@@ -19,6 +19,7 @@ require_once __DIR__.'/../app/AppKernel.php';
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
+Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
--- web/app_dev.php
+++ web/app_dev.php
@@ -21,6 +21,7 @@ require_once __DIR__.'/../app/AppKernel.php';
 
 $kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
+Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();

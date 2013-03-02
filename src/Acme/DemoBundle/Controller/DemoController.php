3550f5cdf2b6ebd761bca61ab80d431c037ced8f
Bertrand Zuchuat <bertrand.zuchuat@gmail.com>
2013-03-02 22:53:37 +0100

    Refactoring isMethod, bind and Flash
    Changed bindRequest by bind

--- src/Acme/DemoBundle/Controller/DemoController.php
+++ src/Acme/DemoBundle/Controller/DemoController.php
@@ -39,14 +39,14 @@ class DemoController extends Controller
         $form = $this->get('form.factory')->create(new ContactType());
 
         $request = $this->get('request');
-        if ('POST' == $request->getMethod()) {
-            $form->bindRequest($request);
+        if ($request->isMethod('POST')) {
+            $form->bind($request);
             if ($form->isValid()) {
                 $mailer = $this->get('mailer');
                 // .. setup a message and send it
                 // http://symfony.com/doc/current/cookbook/email.html
 
-                $this->get('session')->setFlash('notice', 'Message sent!');
+                $this->get('session')->getFlashBag()->set('notice', 'Message sent!');
 
                 return new RedirectResponse($this->generateUrl('_demo'));
             }

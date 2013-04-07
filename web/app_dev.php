494543ea2c9e32be2566ea73a56ef12b6a758019
Fabien Potencier <fabien.potencier@gmail.com>
2013-04-07 18:13:03 +0200

    made changes needed for the introduction of the Debug component

--- web/app_dev.php
+++ web/app_dev.php
@@ -1,6 +1,7 @@
 <?php
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
 
 // If you don't want to setup permissions the proper way, just uncomment the following PHP line
 // read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
@@ -17,6 +18,8 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
 }
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+Debug::enable();
+
 require_once __DIR__.'/../app/AppKernel.php';
 
 $kernel = new AppKernel('dev', true);

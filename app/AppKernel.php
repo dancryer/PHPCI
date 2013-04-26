d84850acafb291bd3ae15e87c6b5aa389fe810cf
Fabien Potencier <fabien.potencier@gmail.com>
2013-04-26 09:12:36 +0200

    removed non-MIT/BSD licensed bundles and librairies

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,9 +16,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new JMS\AopBundle\JMSAopBundle(),
-            new JMS\DiExtraBundle\JMSDiExtraBundle($this),
-            new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {

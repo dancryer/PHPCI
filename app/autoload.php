f3aa1a753f9866adda103d1e1cbc1b2e496d35c2
Fabien Potencier <fabien.potencier@gmail.com>
2013-04-30 19:56:58 +0200

    removed obsolete code (managed by Composer now -- closes #539)

--- app/autoload.php
+++ app/autoload.php
@@ -8,11 +8,6 @@ use Composer\Autoload\ClassLoader;
  */
 $loader = require __DIR__.'/../vendor/autoload.php';
 
-// intl
-if (!function_exists('intl_get_error_code')) {
-    require_once __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs/functions.php';
-}
-
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
 return $loader;

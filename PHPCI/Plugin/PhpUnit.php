1b93921cd012c8b3797ae7f17186377ff7022fdd
meadsteve <meadsteve@gmail.com>
2013-05-15 13:01:47 +0100

    Added argument to Phpunit plugin: config.  This takes the path to an xml configuration file which will be run instead of the specified directory.

--- PHPCI/Plugin/PhpUnit.php
+++ PHPCI/Plugin/PhpUnit.php
@@ -8,19 +8,30 @@ class PhpUnit implements \PHPCI\Plugin
 	protected $args;
 	protected $phpci;
 
+/**
+ * @var string $xmlConfigFile The path of an xml config for PHPUnit
+ */
+	protected $xmlConfigFile;
+
 	public function __construct(\PHPCI\Builder $phpci, array $options = array())
 	{
 		$this->phpci		= $phpci;
 		$this->directory	= isset($options['directory']) ? $options['directory'] : $phpci->buildPath;
+		$this->xmlConfigFile = isset($options['config']) ? $options['config'] : null;
 		$this->args			= isset($options['args']) ? $options['args'] : '';
 	}
 
 	public function execute()
 	{
-		$curdir = getcwd();
-		chdir($this->phpci->buildPath);
-		$success = $this->phpci->executeCommand(PHPCI_BIN_DIR . 'phpunit ' . $this->args . ' ' . $this->phpci->buildPath . $this->directory);
-		chdir($curdir);
+		if ($this->xmlConfigFile === null) {
+			$curdir = getcwd();
+			chdir($this->phpci->buildPath);
+			$success = $this->phpci->executeCommand(PHPCI_BIN_DIR . 'phpunit ' . $this->args . ' ' . $this->phpci->buildPath . $this->directory);
+			chdir($curdir);
+		}
+		else {
+			$success = $this->phpci->executeCommand(PHPCI_BIN_DIR . 'phpunit ' . $this->args . ' -c ' . $this->phpci->buildPath . $this->xmlConfigFile );
+		}
 		
 		return $success;
 	}

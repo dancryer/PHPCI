abcc5f13d81590f7f47a838c82a571163dcb6295
Steve Kamerman <stevekamerman@gmail.com>
2013-05-16 14:20:40 -0400

    Fixed GitHub Remote SSH build authentication errors

--- PHPCI/Model/Build/RemoteGitBuild.php
+++ PHPCI/Model/Build/RemoteGitBuild.php
@@ -76,7 +76,7 @@ abstract class RemoteGitBuild extends Build
         chmod($keyFile, 0600);
 
         // Use the key file to do an SSH clone:
-        $cmd = 'ssh-agent ssh-add "%s" && git clone -b %s %s "%s" && ssh-agent -k';
+        $cmd = 'eval `ssh-agent -s` && ssh-add "%s" && git clone -b %s %s "%s" && ssh-agent -k';
         $success = $builder->executeCommand($cmd, $keyFile, $this->getBranch(), $this->getCloneUrl(), $to);
         
         // Remove the key file:

43b3c146a20423f5a8ae8b8560d5df9b82e8d789
Jordi Boggiano <j.boggiano@seld.be>
2013-07-10 13:34:28 +0200

    Unregister initial loader when using ApcClassLoader

--- web/app.php
+++ web/app.php
@@ -9,8 +9,9 @@ $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 // Change 'sf2' to a unique prefix in order to prevent cache key conflicts
 // with other applications also using APC.
 /*
-$loader = new ApcClassLoader('sf2', $loader);
-$loader->register(true);
+$apcLoader = new ApcClassLoader('sf2', $loader);
+$loader->unregister();
+$apcLoader->register(true);
 */
 
 require_once __DIR__.'/../app/AppKernel.php';

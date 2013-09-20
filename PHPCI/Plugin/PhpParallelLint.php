12ee81a888f64755f00c931fbfd30ed279ceb40a
Václav Makeš <info@makes.cz>
2013-09-20 12:57:23 +0200

    Added initial version of the PHP parallel lint plugin.

---
+++ PHPCI/Plugin/PhpParallelLint.php
@@ -0,0 +1,42 @@
+<?php
+/**
+* PHPCI - Continuous Integration for PHP
+*
+* @copyright    Copyright 2013, Block 8 Limited.
+* @license      https://github.com/Block8/PHPCI/blob/master/LICENSE.md
+* @link         http://www.phptesting.org/
+*/
+
+namespace PHPCI\Plugin;
+
+/**
+* Php Parallel Lint Plugin - Provides access to PHP lint functionality.
+* @author       Vaclav Makes <vaclav@makes.cz>
+* @package      PHPCI
+* @subpackage   Plugins
+*/
+class PhpParallelLint implements \PHPCI\Plugin
+{
+    protected $directory;
+    protected $preferDist;
+    protected $phpci;
+
+    public function __construct(\PHPCI\Builder $phpci, array $options = array())
+    {
+        $path               = $phpci->buildPath;
+        $this->phpci        = $phpci;
+        $this->directory    = isset($options['directory']) ? $path . $options['directory'] : $path;
+    }
+
+    /**
+    * Executes parallel lint
+    */
+    public function execute()
+    {
+        // build the parallel lint command
+        $cmd = "run %s";
+
+        // and execute it
+        return $this->phpci->executeCommand(PHPCI_BIN_DIR . $cmd, $this->directory);
+    }
+}

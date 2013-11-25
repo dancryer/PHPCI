03a81a3283d73ae55bf98c79e7b29075dc10643c
meadsteve <meadsteve@gmail.com>
2013-11-25 21:19:48 +0000

    fix whitespace.

--- Tests/PHPCI/Plugin/PHPUnitTest.php
+++ Tests/PHPCI/Plugin/PHPUnitTest.php
@@ -1,71 +1,73 @@
 <?php
 /**
-* PHPCI - Continuous Integration for PHP
-*
-* @copyright	Copyright 2013, Block 8 Limited.
-* @license		https://github.com/Block8/PHPCI/blob/master/LICENSE.md
-* @link			http://www.phptesting.org/
-*/
+ * PHPCI - Continuous Integration for PHP
+ *
+ * @copyright    Copyright 2013, Block 8 Limited.
+ * @license        https://github.com/Block8/PHPCI/blob/master/LICENSE.md
+ * @link            http://www.phptesting.org/
+ */
 
 namespace PHPCI\Plugin\Tests;
+
 use PHPCI\Plugin\PHPUnit;
 
 /**
-* Unit test for the PHPUnit plugin.
-* @author meadsteve
-*/
-class PHPUnitTest extends  \PHPUnit_Framework_TestCase
+ * Unit test for the PHPUnit plugin.
+ * @author meadsteve
+ */
+class PHPUnitTest extends \PHPUnit_Framework_TestCase
 {
 
-	/**
-	 * @var PHPUnit $testedPhpUnit
-	 */
-	protected $testedPhpUnit;
-
-	/**
-	 * @var \PHPUnit_Framework_MockObject_MockObject $mockCiBuilder
-	 */
-	protected $mockCiBuilder;
+    /**
+     * @var PHPUnit $testedPhpUnit
+     */
+    protected $testedPhpUnit;
 
     /**
-	 * @var \PHPUnit_Framework_MockObject_MockObject $mockCiBuilder
-	 */
-	protected $mockBuild;
+     * @var \PHPUnit_Framework_MockObject_MockObject $mockCiBuilder
+     */
+    protected $mockCiBuilder;
 
-	public function setUp()
-	{
-		$this->mockCiBuilder = $this->getMock(
-			'\PHPCI\Builder',
-			array(),
-			array(),
-			"mockBuilder",
-			false
-		);
-		$this->mockCiBuilder->buildPath = "/";
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject $mockCiBuilder
+     */
+    protected $mockBuild;
+
+    public function setUp()
+    {
+        $this->mockCiBuilder = $this->getMock(
+            '\PHPCI\Builder',
+            array('findBinary'),
+            array(),
+            "mockBuilder",
+            false
+        );
+        $this->mockCiBuilder->buildPath = "/";
 
         $this->mockBuild = $this->getMock(
-			'\PHPCI\Model\Build',
-			array(),
-			array(),
-			"MockBuild",
-			false
-		);
+            '\PHPCI\Model\Build',
+            array(),
+            array(),
+            "MockBuild",
+            false
+        );
 
-		$this->loadPhpUnitWithOptions();
-	}
+        $this->loadPhpUnitWithOptions();
+    }
 
-	protected function loadPhpUnitWithOptions($arrOptions = array())
-	{
-		$this->testedPhpUnit = new PHPUnit($this->mockCiBuilder,$this->mockBuild, $arrOptions);
-	}
+    protected function loadPhpUnitWithOptions($arrOptions = array())
+    {
+        $this->testedPhpUnit = new PHPUnit($this->mockCiBuilder, $this->mockBuild, $arrOptions);
+    }
 
     /**
      * @param \PHPUnit_Framework_MockObject_Matcher_Invocation $expectation
      */
-    protected function expectFindBinaryToBeCalled($expectation) {
+    protected function expectFindBinaryToBeCalled($expectation)
+    {
         $this->mockCiBuilder->expects($expectation)
-                            ->method("findBinary")
-                            ->will($this->returnValue("phpunit"));
+            ->method("findBinary")
+            ->will($this->returnValue("phpunit"));
     }
 
     /**
@@ -74,89 +76,99 @@ class PHPUnitTest extends  \PHPUnit_Framework_TestCase
     public function expectExectuteCommandToBeCalled($expectation)
     {
         $this->mockCiBuilder->expects($expectation)
-                            ->method("executeCommand");
+            ->method("executeCommand");
     }
 
-	/**
-	 * @covers PHPUnit::execute
-	 */
-	public function testExecute_ReturnsTrueWithoutArgs()
-	{
-		$returnValue = $this->testedPhpUnit->execute();
-		$expectedReturn = true;
+    /**
+     * @covers PHPUnit::execute
+     */
+    public function testExecute_ReturnsTrueWithoutArgs()
+    {
+        $returnValue = $this->testedPhpUnit->execute();
+        $expectedReturn = true;
 
-		$this->assertEquals($expectedReturn, $returnValue);
-	}
+        $this->assertEquals($expectedReturn, $returnValue);
+    }
 
-	/**
-	 * @covers PHPUnit::execute
-	 * @covers PHPUnit::runDir
-	 */
-	public function testExecute_CallsExecuteCommandOnceWhenGivenStringDirectory()
-	{
-		chdir('/');
+    /**
+     * @covers PHPUnit::execute
+     * @covers PHPUnit::runDir
+     */
+    public function testExecute_CallsExecuteCommandOnceWhenGivenStringDirectory(
+    )
+    {
+        chdir('/');
 
-		$this->loadPhpUnitWithOptions(array(
-			'directory'	=> "Fake/Test/Path"
-		));
-
-        $this->expectFindBinaryToBeCalled($this->once());
-		$this->expectExectuteCommandToBeCalled($this->once());
-
-		$returnValue = $this->testedPhpUnit->execute();
-	}
-
-	/**
-	 * @covers PHPUnit::execute
-	 * @covers PHPUnit::runConfigFile
-	 */
-	public function testExecute_CallsExecuteCommandOnceWhenGivenStringConfig()
-	{
-		chdir('/');
-
-		$this->loadPhpUnitWithOptions(array(
-			'config'	=> "Fake/Test/config.xml"
-		));
+        $this->loadPhpUnitWithOptions(
+            array(
+                'directory' => "Fake/Test/Path"
+            )
+        );
 
         $this->expectFindBinaryToBeCalled($this->once());
         $this->expectExectuteCommandToBeCalled($this->once());
 
         $returnValue = $this->testedPhpUnit->execute();
-	}
+    }
 
-	/**
-	 * @covers PHPUnit::execute
-	 * @covers PHPUnit::runDir
-	 */
-	public function testExecute_CallsExecuteCommandManyTimesWhenGivenArrayDirectory()
-	{
-		chdir('/');
+    /**
+     * @covers PHPUnit::execute
+     * @covers PHPUnit::runConfigFile
+     */
+    public function testExecute_CallsExecuteCommandOnceWhenGivenStringConfig()
+    {
+        chdir('/');
 
-		$this->loadPhpUnitWithOptions(array(
-			'directory'	=> array(0, 1)
-		));
+        $this->loadPhpUnitWithOptions(
+            array(
+                'config' => "Fake/Test/config.xml"
+            )
+        );
 
-		$this->mockCiBuilder->expects($this->at(0))->method("executeCommand");
-		$this->mockCiBuilder->expects($this->at(1))->method("executeCommand");
+        $this->expectFindBinaryToBeCalled($this->once());
+        $this->expectExectuteCommandToBeCalled($this->once());
 
-		$returnValue = $this->testedPhpUnit->execute();
-	}
+        $returnValue = $this->testedPhpUnit->execute();
+    }
 
-	/**
-	 * @covers PHPUnit::execute
-	 * @covers PHPUnit::runConfigFile
-	 */
-	public function testExecute_CallsExecuteCommandManyTimesWhenGivenArrayConfig()
-	{
-		chdir('/');
+    /**
+     * @covers PHPUnit::execute
+     * @covers PHPUnit::runDir
+     */
+    public function testExecute_CallsExecuteCommandManyTimesWhenGivenArrayDirectory(
+    )
+    {
+        chdir('/');
 
-		$this->loadPhpUnitWithOptions(array(
-			'config'	=> array(0, 1)
-		));
+        $this->loadPhpUnitWithOptions(
+            array(
+                'directory' => array(0, 1)
+            )
+        );
 
-		$this->mockCiBuilder->expects($this->at(0))->method("executeCommand");
-		$this->mockCiBuilder->expects($this->at(1))->method("executeCommand");
+        $this->mockCiBuilder->expects($this->at(0))->method("executeCommand");
+        $this->mockCiBuilder->expects($this->at(1))->method("executeCommand");
 
-		$returnValue = $this->testedPhpUnit->execute();
-	}
+        $returnValue = $this->testedPhpUnit->execute();
+    }
+
+    /**
+     * @covers PHPUnit::execute
+     * @covers PHPUnit::runConfigFile
+     */
+    public function testExecute_CallsExecuteCommandManyTimesWhenGivenArrayConfig()
+    {
+        chdir('/');
+
+        $this->loadPhpUnitWithOptions(
+            array(
+                'config' => array(0, 1)
+            )
+        );
+
+        $this->mockCiBuilder->expects($this->at(0))->method("executeCommand");
+        $this->mockCiBuilder->expects($this->at(1))->method("executeCommand");
+
+        $returnValue = $this->testedPhpUnit->execute();
+    }
 }

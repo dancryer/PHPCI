ee9871e4a8e8b10bf4abee1878f514eb2db5cec8
meadsteve <meadsteve@gmail.com>
2013-11-28 21:36:56 +0000

    add tests to LoggerConfig for handler attaching.

--- Tests/PHPCI/Helper/LoggerConfigTest.php
+++ Tests/PHPCI/Helper/LoggerConfigTest.php
@@ -1,19 +1,75 @@
 <?php
 
+use \PHPCI\Helper\LoggerConfig;
+
 class LoggerConfigTest extends PHPUnit_Framework_TestCase
 {
     public function testGetFor_ReturnsPSRLogger()
     {
-        $config = new \PHPCI\Helper\LoggerConfig(array());
+        $config = new LoggerConfig(array());
         $logger = $config->getFor("something");
         $this->assertInstanceOf('\Psr\Log\LoggerInterface', $logger);
     }
 
     public function testGetFor_ReturnsMonologInstance()
     {
-        $config = new \PHPCI\Helper\LoggerConfig(array());
+        $config = new LoggerConfig(array());
         $logger = $config->getFor("something");
         $this->assertInstanceOf('\Monolog\Logger', $logger);
+    }
+
+    public function testGetFor_AttachesAlwaysPresentHandlers()
+    {
+        $expectedHandler = new Monolog\Handler\NullHandler();
+        $config = new LoggerConfig(array(
+            LoggerConfig::KEY_AlwaysLoaded => function() use ($expectedHandler) {
+                return array($expectedHandler);
+            }
+        ));
+
+        /** @var \Monolog\Logger $logger */
+        $logger = $config->getFor("something");
+        $actualHandler = $logger->popHandler();
+
+        $this->assertEquals($expectedHandler, $actualHandler);
+    }
+
+    public function testGetFor_AttachesSpecificHandlers()
+    {
+        $expectedHandler = new Monolog\Handler\NullHandler();
+        $config = new LoggerConfig(array(
+            "Specific" => function() use ($expectedHandler) {
+                return array($expectedHandler);
+            }
+        ));
+
+        /** @var \Monolog\Logger $logger */
+        $logger = $config->getFor("Specific");
+        $actualHandler = $logger->popHandler();
+
+        $this->assertSame($expectedHandler, $actualHandler);
+    }
+
+    public function testGetFor_IgnoresAlternativeHandlers()
+    {
+        $expectedHandler = new Monolog\Handler\NullHandler();
+        $alternativeHandler = new Monolog\Handler\NullHandler();
+
+        $config = new LoggerConfig(array(
+            "Specific" => function() use ($expectedHandler) {
+                return array($expectedHandler);
+            },
+            "Other" => function() use ($alternativeHandler) {
+                return array($alternativeHandler);
+            }
+        ));
+
+        /** @var \Monolog\Logger $logger */
+        $logger = $config->getFor("Specific");
+        $actualHandler = $logger->popHandler();
+
+        $this->assertSame($expectedHandler, $actualHandler);
+        $this->assertNotSame($alternativeHandler, $actualHandler);
     }
 }
  

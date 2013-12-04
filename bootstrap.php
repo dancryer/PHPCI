22fecc2d1919f4e683a9ac8808bd6bd282d9100d
Haydar Ciftci <haydar.ciftci@gmail.com>
2013-12-04 12:18:56 +0100

    Fixed: Redirect not working when PHPCI is not installed in the server root

--- bootstrap.php
+++ bootstrap.php
@@ -27,7 +27,7 @@ $autoload = function ($class) {
 
 spl_autoload_register($autoload, true, true);
 
-if (!file_exists(dirname(__FILE__) . '/PHPCI/config.yml') && (!defined('PHPCI_IS_CONSOLE') || !PHPCI_IS_CONSOLE) && $_SERVER['PHP_SELF'] != '/install.php') {
+if (!file_exists(dirname(__FILE__) . '/PHPCI/config.yml') && (!defined('PHPCI_IS_CONSOLE') || !PHPCI_IS_CONSOLE) && substr($_SERVER['PHP_SELF'], -12) != '/install.php') {
     header('Location: install.php');
     die;
 }

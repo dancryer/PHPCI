471f993b6cbb2b1b6ac0f3e56c11513d2f7650b3
Libbux <j@thelibbster.com>
2014-07-13 11:24:40 +0100

    Fix issue Block8/PHPCI#484 - implement method in Project model

--- PHPCI/Model/Build/GitlabBuild.php
+++ PHPCI/Model/Build/GitlabBuild.php
@@ -26,7 +26,7 @@ class GitlabBuild extends RemoteGitBuild
     public function getCommitLink()
     {
         $domain = $this->getProject()->getAccessInformation("domain");
-        return 'http://' . $domain . '/' . $this->getProject()->getReference(true) . '/commit/' . $this->getCommitId();
+        return 'http://' . $domain . '/' . $this->getProject()->getReferenceForLink() . '/commit/' . $this->getCommitId();
     }
 
     /**
@@ -35,7 +35,7 @@ class GitlabBuild extends RemoteGitBuild
     public function getBranchLink()
     {
         $domain = $this->getProject()->getAccessInformation("domain");
-        return 'http://' . $domain . '/' . $this->getProject()->getReference(true) . '/tree/' . $this->getBranch();
+        return 'http://' . $domain . '/' . $this->getProject()->getReferenceForLink() . '/tree/' . $this->getBranch();
     }
 
     /**

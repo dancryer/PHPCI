2df0e6fe255c8e6aa4f585574c482ac7607b6dd5
Fabien Potencier <fabien.potencier@gmail.com>
2014-07-19 10:54:44 +0200

    added back a comment that aas removed in the previous merge

--- web/app.php
+++ web/app.php
@@ -5,7 +5,10 @@ use Symfony\Component\HttpFoundation\Request;
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
-// Use APC for autoloading to improve performance.
+// Enable APC for autoloading to improve performance.
+// You should change the ApcClassLoader first argument to a unique prefix
+// in order to prevent cache key conflicts with other applications
+// also using APC.
 /*
 $apcLoader = new ApcClassLoader(sha1(__FILE__), $loader);
 $loader->unregister();

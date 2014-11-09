984ed05c387f4035c0bb0e165e21a30a4f441d5f
Jimmy Cleuren <jimmy.cleuren@cegeka.be>
2014-11-09 18:05:15 +0100

    remove other changes

--- PHPCI/Command/PollCommand.php
+++ PHPCI/Command/PollCommand.php
@@ -75,7 +75,6 @@ class PollCommand extends Command
 
             $last_commit = $commits['body'][0]['sha'];
             $last_committer = $commits['body'][0]['commit']['committer']['email'];
-            $message = $commits['body'][0]['commit']['message'];
 
             $this->logger->info("Last commit to github for " . $project->getTitle() . " is " . $last_commit);
 
@@ -90,7 +89,6 @@ class PollCommand extends Command
                 $build->setStatus(Build::STATUS_NEW);
                 $build->setBranch($project->getBranch());
                 $build->setCreated(new \DateTime());
-		$build->setCommitMessage($message);
                 if (!empty($last_committer)) {
                     $build->setCommitterEmail($last_committer);
                 }

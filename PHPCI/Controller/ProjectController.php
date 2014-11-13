2ab45e28396d12df1328c63615def3a0980109d4
Artjom Kurapov <artkurapov@gmail.com>
2014-11-13 17:14:30 +0200

    #543 Branch checking if directory exist
    Removed check for repository name to be a directory. Repository for local paths is not relevant
    #todo remove repository from being required at all for local setup

--- PHPCI/Controller/ProjectController.php
+++ PHPCI/Controller/ProjectController.php
@@ -397,8 +397,6 @@ class ProjectController extends \PHPCI\Controller
 
             if (in_array($type, $validators) && !preg_match($validators[$type]['regex'], $val)) {
                 throw new \Exception($validators[$type]['message']);
-            } elseif ($type == 'local' && !is_dir($val)) {
-                throw new \Exception('The path you specified does not exist.');
             }
 
             return true;

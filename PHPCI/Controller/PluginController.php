f58f2a8129268b990dd530ee1e807abdab67e9c0
Artjom Kurapov <artkurapov@gmail.com>
2014-11-17 14:00:26 +0200

    Update PluginController.php

--- PHPCI/Controller/PluginController.php
+++ PHPCI/Controller/PluginController.php
@@ -39,7 +39,7 @@ class PluginController extends \PHPCI\Controller
 
     public function index()
     {
-        if (!$_SESSION['user']->getIsAdmin()) {
+        if (!\PHPCI\Helper\Session['user']->getIsAdmin()) {
             throw new \Exception('You do not have permission to do that.');
         }
 
@@ -67,7 +67,7 @@ class PluginController extends \PHPCI\Controller
 
     public function remove()
     {
-        if (!$_SESSION['user']->getIsAdmin()) {
+        if (!\PHPCI\Helper\Session['user']->getIsAdmin()) {
             throw new \Exception('You do not have permission to do that.');
         }
 
@@ -88,7 +88,7 @@ class PluginController extends \PHPCI\Controller
 
     public function install()
     {
-        if (!$_SESSION['user']->getIsAdmin()) {
+        if (!\PHPCI\Helper\Session::get('user')->getIsAdmin()) {
             throw new \Exception('You do not have permission to do that.');
         }
 

70e2fdc3f681db0f5905aa72f529764a80354bd7
Fabien Potencier <fabien.potencier@gmail.com>
2014-11-29 11:09:08 +0100

    feature #728 Added a new AppBundle to comply with Symfony Best Practices (javiereguiluz)
    This PR was squashed before being merged into the 3.0-dev branch (closes #728).
    
    Discussion
    ----------
    
    Added a new AppBundle to comply with Symfony Best Practices
    
    | Q             | A
    | ------------- | ---
    | Bug fix?      | no
    | New feature?  | yes
    | BC breaks?    | no
    | Deprecations? | no
    | Tests pass?   | yes
    | Fixed tickets | #671
    | License       | MIT
    | Doc PR        | -
    
    Symfony Best Practices recommend to start developing your applications with a single AppBundle bundle. This PR allows to include that AppBundle in the Symfony Standard Edition, to make it easier for developers.
    
    I used the same skeleton as for the `generate:bundle` command, but I thought that it would be better to simplify it and remove the `$name` route parameter and use just a static template without parameters.
    
    Commits
    -------
    
    45da0d0 Added a new AppBundle to comply with Symfony Best Practices

---
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AppBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertTrue($crawler->filter('html:contains("Homepage")')->count() > 0);
+    }
+}

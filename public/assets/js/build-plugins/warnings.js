c2c51e376be2e78b5ea1fb73194e22821f11355e
Artjom Kurapov <artkurapov@gmail.com>
2014-12-06 15:47:06 +0200

    Logarithmic vertical scale for charts
    Since we have one diagram for multiple results, we may have PHPCS result giving 12000 errors, while PHPUnit gives 200 tests, and 1 failed test.. those numbers and their variation won't be very visible, unless we use logarithmic scale
    
    Closes #686

--- public/assets/js/build-plugins/warnings.js
+++ public/assets/js/build-plugins/warnings.js
@@ -94,7 +94,7 @@ var warningsPlugin = ActiveBuild.UiPlugin.extend({
         var data = google.visualization.arrayToDataTable(data);
         var options = {
             hAxis: {title: 'Builds'},
-            vAxis: {title: 'Warnings / Errors'},
+            vAxis: {title: 'Warnings / Errors', logScale:true},
             backgroundColor: { fill: 'transparent' },
             height: 275,
             pointSize: 3

c4e09856d86979e5031e745ab102d58fbba594ad
Stéphane HULARD <s.hulard@chstudio.fr>
2014-12-09 10:33:26 +0100

    Update language detection behaviour
    My firefox is HTTP_ACCEPT_LANGUAGE: fr,en-US;q=0.7,en;q=0.3
    fr is a valid language so, we need to handle the HTTP_ACCEPT_LANGUAGE in a different way.
    
    I build an array sorted by language priority then I load the linked file
    
    Signed-off-by: Stéphane HULARD <s.hulard@chstudio.fr>

--- PHPCI/Helper/Lang.php
+++ PHPCI/Helper/Lang.php
@@ -13,6 +13,7 @@ use b8\Config;
 
 class Lang
 {
+    protected static $languages = array();
     protected static $language = null;
     protected static $strings = array();
 
@@ -40,23 +41,46 @@ class Lang
 
     public static function init(Config $config)
     {
+        //load existing languages
+        self::$languages = array();
+        $langFiles = glob(PHPCI_DIR . 'PHPCI/Languages/lang.*.php');
+        foreach( $langFiles as $file ) {
+            preg_match('/\.([^.]+)\.php$/', $file, $match);
+            self::$languages[$match[1]] = $file;
+        }
+
         // Try user language:
         if (isset($_SERVER) && array_key_exists('HTTP_ACCEPT_LANGUAGE', $_SERVER)) {
             $matches = array();
+            if( preg_match_all('/([a-z]{1,8}(-[a-z]{1,8})?)\s*(;\s*q\s*=\s*(1|0\.[0-9]+))?/i', $_SERVER['HTTP_ACCEPT_LANGUAGE'], $matches) ) {
+                if (count($matches[1])) {
+                    // create a list like "en" => 0.8
+                    $langs = array_combine($matches[1], $matches[4]);
 
-            if (preg_match('/([a-zA-Z]{2}\-[a-zA-Z]{2})/', $_SERVER['HTTP_ACCEPT_LANGUAGE'], $matches)) {
-                self::$language = strtolower($matches[1]);
-                self::$strings = self::loadLanguage();
+                    // set default to 1 for any without q factor
+                    foreach ($langs as $lang => $val) {
+                        if ($val === '') $langs[$lang] = 1;
+                    }
 
-                if (!is_null(self::$strings)) {
-                    return;
+                    // sort list based on value
+                    arsort($langs, SORT_NUMERIC);
+                }
+
+                foreach( $langs as $code => $factor ) {
+                    if( isset(self::$languages[$code]) ) {
+                        self::$language = strtolower($code);
+                        self::$strings = self::loadLanguage();
+
+                        if (!is_null(self::$strings)) {
+                            return;
+                        }
+                    }
                 }
             }
         }
 
         // Try the installation default language:
         self::$language = $config->get('phpci.default_language', null);
-
         if (!is_null(self::$language)) {
             self::$strings = self::loadLanguage();
 

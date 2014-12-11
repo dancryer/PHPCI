f78b6ae9f99a4aaded5ae15b961fc0001fef925f
Stephen Ball <REBELinBLUE@users.noreply.github.com>
2014-12-11 14:41:00 +0000

    Cleanup of the disabled login mode.
    Hides user menu, the navigation user panel, etc.
    
    Closes #718

---
+++ PHPCI/Helper/LoginIsDisabled.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * PHPCI - Continuous Integration for PHP
+ *
+ * @copyright    Copyright 2014, Block 8 Limited.
+ * @license      https://github.com/Block8/PHPCI/blob/master/LICENSE.md
+ * @link         https://www.phptesting.org/
+ */
+
+namespace PHPCI\Helper;
+
+/**
+* Login Is Disabled Helper - Checks if login is disalbed in the view
+* @author       Stephen Ball <phpci@stephen.rebelinblue.com>
+* @package      PHPCI
+* @subpackage   Web
+*/
+class LoginIsDisabled
+{
+    /**
+     * Checks if 
+     * @param $method
+     * @param array $params
+     * @return mixed|null
+     */
+    public function __call($method, $params = array())
+    {
+        unset($method, $params);
+        
+        $config = b8\Config::getInstance();
+        $state = (bool) $config->get('phpci.authentication_settings.state', false);
+
+        return (false !== $state);
+    }
+}

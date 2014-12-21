223e6c89905a4b064e93c2276631c214fa081166
Alex Pierstoval <pierstoval@gmail.com>
2014-12-21 21:55:35 +0100

    Issue #758 Modified default route for AppBundle

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,7 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/app/example", name="homepage")
      */
     public function indexAction()
     {

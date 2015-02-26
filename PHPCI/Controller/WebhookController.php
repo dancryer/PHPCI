60131ae7b6a9cdf0c7660ce6cfc98ffd69af98ad
Martin Sadovy <sodae@email.cz>
2015-02-26 19:59:49 +0100

    Github: Support pull private repository from pull request
    Closes #832

--- PHPCI/Controller/WebhookController.php
+++ PHPCI/Controller/WebhookController.php
@@ -260,12 +260,14 @@ class WebhookController extends \b8\Controller
                 $committer = $commit['commit']['author']['email'];
                 $message = $commit['commit']['message'];
 
+                $remoteUrlKey = $payload['pull_request']['head']['repo']['private'] ? 'ssh_url' : 'clone_url';
+
                 $extra = array(
                     'build_type' => 'pull_request',
                     'pull_request_id' => $payload['pull_request']['id'],
                     'pull_request_number' => $payload['number'],
                     'remote_branch' => $payload['pull_request']['head']['ref'],
-                    'remote_url' => $payload['pull_request']['head']['repo']['clone_url'],
+                    'remote_url' => $payload['pull_request']['head']['repo'][$remoteUrlKey],
                 );
 
                 $results[$id] = $this->createBuild($project, $id, $branch, $committer, $message, $extra);

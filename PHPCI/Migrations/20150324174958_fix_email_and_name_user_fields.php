8d25e2142f3cfb13d394cdffda76b6fa00644194
corpsee <poisoncorpsee@gmail.com>
2015-03-24 23:56:40 +0600

    Added unique indexes for email and name user table fields (migration)

---
+++ PHPCI/Migrations/20150324174958_fix_email_and_name_user_fields.php
@@ -0,0 +1,30 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class FixEmailAndNameUserFields extends AbstractMigration
+{
+    /**
+     * Migrate Up.
+     */
+    public function up()
+    {
+        $user_table = $this->table('user');
+        $user_table
+            ->addIndex('email', ['unique' => true])
+            ->addIndex('name', ['unique' => true])
+            ->save();
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+        $user_table = $this->table('user');
+        $user_table
+            ->removeIndex('email', ['unique' => true])
+            ->removeIndex('name', ['unique' => true])
+            ->save();
+    }
+}

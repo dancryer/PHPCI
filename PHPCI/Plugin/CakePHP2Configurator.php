e7585a4f52fc388a85944777f7f8cd1433c9794e
Esteban Zeller <tranfuga25s@users.noreply.github.com>
2015-04-07 14:33:01 -0300

    Update CakePHP2Configurator.php

--- PHPCI/Plugin/CakePHP2Configurator.php
+++ PHPCI/Plugin/CakePHP2Configurator.php
@@ -13,8 +13,6 @@ use PHPCI;
 use PHPCI\Builder;
 use PHPCI\Helper\Lang;
 use PHPCI\Model\Build;
-use PHPCI\Plugin\Util\TapParser;
-
 /**
  * CakePHP2 Plugin - sets the auto configuration for the application
  *
@@ -71,14 +69,17 @@ class CakePHP2Configurator implements PHPCI\Plugin, PHPCI\ZeroConfigPlugin
     {
         if (count($this->options) == 0 ) {
             // No configuration set
+			$this->phpci->log("Could not found any options to create the configuration.");
+			$this->phpci->logSuccess("Not creating any configuration for this project");
             return true;
         }
 
         if (!array_key_exists('app', $this->options)) {
-        	if (!file_exists('app')) {
-			$this->phpci->logError("Could not found the app directory to create the configuration. Plase specifie it on the project configuration.");
-			return false;
-		}
+			if (!file_exists('app')) {
+				$this->phpci->log(print_r($this->options, true));
+				$this->phpci->logFailure("Could not found the app directory to create the configuration. Plase specify it on the project configuration.");
+				return false;
+			}
             $this->phpci->log("Using app/ as app dir");
             $this->options['app'] = 'app';
         }
@@ -86,6 +87,8 @@ class CakePHP2Configurator implements PHPCI\Plugin, PHPCI\ZeroConfigPlugin
         // Database file
         $configDir = $this->phpci->buildPath;
         $configDir .= $this->options['app'];
+		$configDir .= DIRECTORY_SEPARATOR;
+		$configDir .= 'Config';
 
         // Generate config
         $database_file = $configDir.DIRECTORY_SEPARATOR."database.php";
@@ -95,13 +98,13 @@ class CakePHP2Configurator implements PHPCI\Plugin, PHPCI\ZeroConfigPlugin
                 $this->phpci->log("Creating database configuration");
                 $string = $this->generateDatabaseConfig($this->options['database']);
                 if (file_put_contents($database_file, $string) === FALSE) {
-                    $this->phpci->logError("Could not write the database.php file");
+                    $this->phpci->logFailure("Could not write the database.php file");
                     return false;
                 } else {
                     $this->phpci->logSuccess("Writed file ".$database_file." with database configuration");
                 }
             } else {
-                $this->phpci->logError("There is no database configuration for the application");
+                $this->phpci->logFailure("There is no database configuration for the application");
                 return false;
             }
         }
@@ -116,16 +119,61 @@ class CakePHP2Configurator implements PHPCI\Plugin, PHPCI\ZeroConfigPlugin
                     $this->phpci->log("Creating email configuration");
                     $string = $this->generateEmailConfig($this->options['email']);
                     if (file_put_contents($email_file, $string) === FALSE) {
-                        $this->phpci->logError("Could not write the email.php file");
+                        $this->phpci->logFailure("Could not write the email.php file");
                         return false;
                     } else {
-                        $this->phpci->logSuccess("Writed file ".$email_file." with database configuration");
+                        $this->phpci->logSuccess("Writed file ".$email_file." with email configuration");
                     }
                 }
             } else {
-                $this->phpci->logError("There is no email configuration for the application");
+                $this->phpci->logFailure("There is no email configuration for the application");
             }
         }
+		
+		
+		// Temporary files and folders
+		$tmp_path = $this->phpci->buildPath;
+        $tmp_path .= $this->options['app'];
+		$tmp_path .= DIRECTORY_SEPARATOR;
+		$tmp_path .= 'tmp';
+		if (!file_exists($tmp_path)) {
+			$this->phpci->log("No tmp folder inside app found");
+			if (array_key_exists('tmp', $this->options) &&
+			    $this->options['tmp']['create'] == false) {
+				$this->phpci->log("Not creating temporal folder");
+			} else {
+				$this->phpci->log("Creating temporal folder");
+				$folders[] = '';
+				$folders[] = 'cache';
+				$folders[] = 'logs';
+				$folders[] = 'cache/models';
+				$folders[] = 'cache/persistent';
+				foreach( $folders as $folder) {
+					if (mkdir($tmp_path.DIRECTORY_SEPARATOR.$folder, 0777)) {
+						$this->phpci->logSuccess("Created folder: ".$tmp_path.DIRECTORY_SEPARATOR.$folder);
+					} else {
+						$this->phpci->logFailure("Could not create folder: ".$tmp_path.DIRECTORY_SEPARATOR.$folder);
+					}
+				}
+			}
+		}
+		
+		if (!file_exists($configDir.DIRECTORY_SEPARATOR.'core.php')) {
+			$this->phpci->logFailure("There is no core.php file on the project configuration folder!");
+			//return false;
+			if (!copy($configDir.DIRECTORY_SEPARATOR.'core.php.ls1', $configDir.DIRECTORY_SEPARATOR.'core.php')) {
+				$this->phpci->logFailure("There is no bootstrap.php file on the project configuration folder!");
+			}
+		}
+		
+		if (!file_exists($configDir.DIRECTORY_SEPARATOR.'bootstrap.php')) {
+			$this->phpci->logFailure("There is no bootstrap.php file on the project configuration folder!");
+			//return false;
+			if (!copy($configDir.DIRECTORY_SEPARATOR.'bootstrap.php.ls1', $configDir.DIRECTORY_SEPARATOR.'bootstrap.php')) {
+				$this->phpci->logFailure("There is no bootstrap.php file on the project configuration folder!");
+			}
+		}
+		
         return true;
     }
 
@@ -138,8 +186,7 @@ class CakePHP2Configurator implements PHPCI\Plugin, PHPCI\ZeroConfigPlugin
      */
     private function generateDatabaseConfig( $options ) {
         $default_options = array(
-            'datasource' => 'Datasource/Mysql',
-            'persistent' => 'false',
+            'datasource' => 'Database/Mysql',
             'host' => 'localhost',
             'login' => 'test',
             'password' => 'test',
@@ -147,20 +194,29 @@ class CakePHP2Configurator implements PHPCI\Plugin, PHPCI\ZeroConfigPlugin
             'prefix' => '',
             'encoding' => 'utf8',
         );
-        $actual_config = array_merge($default_options, $options);
+        $actual_config = array_merge($default_options, $options['test']);
         $content = <<<EOT
 <?php
 class DATABASE_CONFIG {
-
+				
+	public \$default = array(
+        'datasource' => '{$actual_config['datasource']}',
+		'host' => '{$actual_config['host']}',
+		'login' => '{$actual_config['login']}',
+		'password' => '{$actual_config['password']}',
+		'database' => '{$actual_config['database']}',
+		'prefix' => '{$actual_config['prefix']}',
+		'encoding' => '{$actual_config['encoding']}',
+	);
+				
 	public \$test = array(
-                'datasource' => {$actual_config['datasource']},
-		'persistent' => {$actual_config['persistent']},
-		'host' => {$actual_config['host']},
-		'login' => {$actual_config['login']},
-		'password' => {$actual_config['password']},
-		'database' => {$actual_config['database']},
-		'prefix' => {$actual_config['prefix']},
-		'encoding' => {$actual_config['encoding']},
+        'datasource' => '{$actual_config['datasource']}',
+		'host' => '{$actual_config['host']}',
+		'login' => '{$actual_config['login']}',
+		'password' => '{$actual_config['password']}',
+		'database' => '{$actual_config['database']}',
+		'prefix' => '{$actual_config['prefix']}',
+		'encoding' => '{$actual_config['encoding']}',
 	);
 }
 EOT;
@@ -186,10 +242,10 @@ EOT;
 class EmailConfig {
 
 	public \$default = array(
-		'from' => {$actual_config['from']},
-		'transport' => {$actual_config['transport']},
-                'host' => {$actual_config['host']},
-		'port' => {$actual_config['port']},
+		'from' => '{$actual_config['from']}',
+		'transport' => '{$actual_config['transport']}',
+        'host' => '{$actual_config['host']}',
+		'port' => '{$actual_config['port']}',
 		'log' => true,
 	);
 }

693e1f79e4476ab3810e786300026e63e92252bd
Fabien Potencier <fabien.potencier@gmail.com>
2015-09-16 09:56:06 +0200

    moved to the 3.0 directory structure

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -29,6 +29,21 @@ class AppKernel extends Kernel
         return $bundles;
     }
 
+    public function getRootDir()
+    {
+        return __DIR__;
+    }
+
+    public function getCacheDir()
+    {
+        return dirname(__DIR__).'/var/cache/'.$this->environment;
+    }
+
+    public function getLogDir()
+    {
+        return dirname(__DIR__).'/var/logs';
+    }
+
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
         $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');

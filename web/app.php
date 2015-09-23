f73cd593ae020185e2a9aec29e7418ff715b964b
Fabien Potencier <fabien.potencier@gmail.com>
2015-09-23 10:08:27 +0200

    fixed CS

--- web/app.php
+++ web/app.php
@@ -3,7 +3,7 @@
 use Symfony\Component\HttpFoundation\Request;
 
 /**
- * @var Composer\Autoload\ClassLoader $loader
+ * @var Composer\Autoload\ClassLoader
  */
 $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../app/bootstrap.php.cache';

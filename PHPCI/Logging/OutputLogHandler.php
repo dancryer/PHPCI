1bd24d3a57bf68aa3594f8ec0347297e47b79414
aliaxander <admin@oxcpa.ru>
2016-02-19 21:04:12 +0300

    OutputLogHandler: filter messages according to verbosity level.

--- PHPCI/Logging/OutputLogHandler.php
+++ PHPCI/Logging/OutputLogHandler.php
@@ -10,40 +10,48 @@
 namespace PHPCI\Logging;
 
 use Monolog\Handler\AbstractProcessingHandler;
-use Psr\Log\LogLevel;
+use Monolog\Logger;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * Class OutputLogHandler outputs the build log to the terminal.
+ *
  * @package PHPCI\Logging
  */
 class OutputLogHandler extends AbstractProcessingHandler
 {
+
+    protected static $levels = array(
+        OutputInterface::VERBOSITY_QUIET => Logger::ERROR,
+        OutputInterface::VERBOSITY_NORMAL => Logger::WARNING,
+        OutputInterface::VERBOSITY_VERBOSE => Logger::NOTICE,
+        OutputInterface::VERBOSITY_VERY_VERBOSE => Logger::INFO,
+        OutputInterface::VERBOSITY_DEBUG => Logger::DEBUG,
+    );
     /**
      * @var OutputInterface
      */
     protected $output;
 
     /**
+     * OutputLogHandler constructor.
+     *
      * @param OutputInterface $output
-     * @param bool|string $level
-     * @param bool $bubble
      */
-    public function __construct(
-        OutputInterface $output,
-        $level = LogLevel::INFO,
-        $bubble = true
-    ) {
-        parent::__construct($level, $bubble);
+    public function __construct(OutputInterface $output)
+    {
+        parent::__construct(static::$levels[$output->getVerbosity()]);
+
         $this->output = $output;
     }
 
     /**
      * Write a log entry to the terminal.
+     *
      * @param array $record
      */
     protected function write(array $record)
     {
-        $this->output->writeln((string)$record['formatted']);
+        $this->output->writeln((string) $record['formatted']);
     }
 }

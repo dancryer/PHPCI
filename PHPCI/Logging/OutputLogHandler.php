e74dcce77f62b07d5716e544d6b91b328657009e
aliaxander <admin@oxcpa.ru>
2016-02-19 21:06:35 +0300

    OutputLogHandler: send messages of severity ERROR or higher to stderr.

--- PHPCI/Logging/OutputLogHandler.php
+++ PHPCI/Logging/OutputLogHandler.php
@@ -11,6 +11,7 @@ namespace PHPCI\Logging;
 
 use Monolog\Handler\AbstractProcessingHandler;
 use Monolog\Logger;
+use Symfony\Component\Console\Output\ConsoleOutputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
@@ -52,6 +53,13 @@ class OutputLogHandler extends AbstractProcessingHandler
      */
     protected function write(array $record)
     {
-        $this->output->writeln((string) $record['formatted']);
+
+        if ($record['level'] >= Logger::ERROR && $this->output instanceof ConsoleOutputInterface) {
+            $output = $this->output->getErrorOutput();
+        } else {
+            $output = $this->output;
+        }
+
+        $output->write($record['formatted']);
     }
 }

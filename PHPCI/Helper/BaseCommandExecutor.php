79e278913debaeec33c86c0387883ede7ed0aea2
Justin Dearing <zippy1981@gmail.com>
2016-03-16 20:48:55 -0400

    Update logging of BaseCommandExecutor.php
    The $this->quiet thing seems like a mistake.

--- PHPCI/Helper/BaseCommandExecutor.php
+++ PHPCI/Helper/BaseCommandExecutor.php
@@ -77,9 +77,7 @@ abstract class BaseCommandExecutor implements CommandExecutor
 
         $command = call_user_func_array('sprintf', $args);
 
-        if ($this->quiet) {
-            $this->logger->log('Executing: ' . $command);
-        }
+        $this->logger->log('Executing: ' . $command);
 
         $status = 0;
         $descriptorSpec = array(

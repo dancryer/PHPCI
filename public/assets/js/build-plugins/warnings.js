8c755f23b5541dfb08a36c75839e441a598237a9
Ricardo Vega <ricardo@gumgum.com>
2016-05-27 17:30:35 -0500

    Fixes NaN error when parsing missing data

--- public/assets/js/build-plugins/warnings.js
+++ public/assets/js/build-plugins/warnings.js
@@ -102,7 +102,7 @@ var warningsPlugin = ActiveBuild.UiPlugin.extend({
             var i = 0;
             for (var key in self.keys) {
 
-                self.chartData.datasets[i].data.push(parseInt(self.data[build][key]));
+                self.chartData.datasets[i].data.push(~~(self.data[build][key]));
                 i++;
             }
         }

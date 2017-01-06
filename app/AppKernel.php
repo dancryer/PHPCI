1f2612cf5c21b460d1d3df10a462d78843e62d01
Robin Chalas <robin.chalas@gmail.com>
2017-01-06 16:35:14 +0100

    Register WebServerBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -23,6 +23,10 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+
+            if ('dev' === $this->getEnvironment()) {
+                $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
+            }
         }
 
         return $bundles;

77f588035f3b99e81e839a19f9a1e60a0e17140f
Fabien Potencier <fabien.potencier@gmail.com>
2017-01-08 08:02:33 -0800

    moved SensioGeneratorBundle to dev

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,9 +22,9 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
 
             if ('dev' === $this->getEnvironment()) {
+                $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
                 $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
             }
         }
